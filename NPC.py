6fa9cbe4f0871859fc222ffbfaf7d165b2e0a126
dis446 <dis446@yahoo.com>
2016-02-08 18:25:04 +0300

    So- I forgot to include the new subclasses in the previous commit :/

---
+++ NPC.py
@@ -0,0 +1,22 @@
+class NPC(Human):
+	def __init__(self, first_name, day_of_birth, parent_1, parent_2, age, gender):
+		Human.___init__(self, first_name, day_of_birth, parent_1, parent_2, age, gender)
+		self.scavenging = 0
+		self.days_scavenging = 0
+		self.days_to_scavenge_for = 0
+	def die(self):
+		global people
+		global rooms
+		print(self.name, " has died")
+		# Uses first index since player will always be the first person in the
+		# list, checks if player has died.
+		end = 1  # Ends game since player has died.
+		if self.assigned_room != "":  # Deals with if the person was assigned to any rooms.
+			for x in range(people):  # Fetches the index of the person.
+				if people[x].name == self.name:
+					index = x
+					break
+			for r in rooms:
+				# Removes person from the rooms' assigned records.
+				del r.assigned[index]
+		people.remove(self)
---
+++ Player.py
@@ -0,0 +1,11 @@
+class Player(Human):
+	def __init__(self,):
+		Human.___init__(self,, first_name, day_of_birth, parent_1, parent_2, age, gender)
+		self.medic = 0  # Improves healing capabilities of stimpacks
+		self.crafting = 0 # Chance to hold on to some components when crafting.
+		self.tactician = 0  # Boosts defense
+		self.cooking = 0  # Boosts production level of kitchen.
+		self.barter = 0  # Decreases prices when buying, increases while selling. Goes up to 4
+		self.inspiration = 0 # Boosts production and defense of all inhabitants.
+		self.scrapper = 0 # Boosts chance of finding a component twice during scrapping.
+		self.electrician = 0  # Boosts power production
