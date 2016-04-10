dddff4d1715ffdbe3e7a4eef9dbc99b03ec3ce67
Edward Askew <edward.askew@foonet.me.uk>
2016-04-10 23:55:59 +0100

    Split IO code from stack code and mainloop code
    - GameShell.run() is now a seperate module level function mainloop that takes a
      shell as an optional argument (if not provided it constructs a GameLoop
      instance to use)
    - moved all stack functions to new class ScreenStack
    - screens now take both a GameShell and a ScreenStack as arguments when run
    - updated Menu and MenuItem screens to take 2 arguments

--- gameshell.py
+++ gameshell.py
@@ -4,10 +4,9 @@ from time import sleep
 
 class GameShell(object):
     """
-    Handles text input and out, implements a basic Finite State Machine with
-    a stack for handling different game screens in the 'run' method. Also
-    provides utility functions for displaying text to the player and handling
-    player input - all player IO should be handled by this class
+    Handles text input and output, provides utility functions for displaying
+    text to the player and handling player input - all player IO should be
+    handled by this class
     """
 
     def __init__(self):
@@ -65,54 +64,7 @@ class GameShell(object):
             print(indent_str + str(line), end=end, flush=True)
             self._sleep(speed)
 
-    def run(self, screen):
-        """
-        Runs the shell starting from screen
 
-        Arguments:
-        screen -- screen to start from, screens should be callables that return
-                  take the GameShell as an argument and return the next screen to be
-                  displayed or None. Returning None will run the screen at the
-                  top of the screen stack or exit if the screen stack is empty
-        """
-        while True:
-            if screen is None:
-                try:
-                    screen = self.pop()
-                except IndexError:
-                    break
-            self.current_screen = screen
-            if hasattr(screen, 'complete'):
-                self.set_completer(screen.complete())
-            try:
-                screen = screen(self)
-            except EOFError:
-                #if we recieve EOF we take this as a sign we want to move back a
-                #screen
-                self.print_line()
-                screen = None
-
-    def push(self, screen=None):
-        """
-        Pushes screen onto the screen stack
-
-        Arguments:
-        screen -- screen to push or None. If none it will push the current
-                  screen onto the stack
-        """
-        if screen is None:
-            screen = self.current_screen
-        self.screen_stack.append(screen)
-
-    def pop(self):
-        """
-        Pop a screen from the screen stack and return it. Throws IndexError
-        if the stack is empty
-
-        Returns:
-        screen -- the screen from the top of the stack.
-        """
-        return self.screen_stack.pop()
 
     def choose_from(self, text, choices):
         """
@@ -215,7 +167,75 @@ class GameShell(object):
         interval = self.intervals[speed]
         sleep(interval)
 
+class ScreenStack(object):
+    """
+    Class to hold a stack of screens
+    """
+    def __init__(self):
+        self._stack = []
+        self.current_screen = None
+
+    def push(self, screen=None):
+        """
+        Pushes screen onto the screen stack
+
+        Arguments:
+        screen -- screen to push or None. If none it will push the current
+                  screen onto the stack
+        """
+        if screen is None:
+            screen = self.current_screen
+        self._stack.append(screen)
+
+    def pop(self):
+        """
+        Pop a screen from the stack and return it. Throws IndexError
+        if the stack is empty
+
+        Returns:
+        screen -- the screen from the top of the stack.
+        """
+        return self._stack.pop()
+
+def mainloop(screen, shell=None):
+    """
+    Runs the mainloop starting from screen. Implements a basic Finite State Machine with
+    a stack for handling different game screens.
+
+    Arguments:
+    screen -- screen to start from, screens should be callables that take the
+              game shell and screen stack as arguments and return the next
+              screen to be displayed or None. Returning None will run the
+              screen at the top of the screen stack or exit if the screen stack
+              is empty
+    shell -- GameShell instance to use for IO, if not provide a new instance of
+             GameShell will be created
+    """
+
+    if shell is None:
+        shell = GameShell()
+    screen_stack = ScreenStack()
+
+    while True:
+        if screen is None:
+            try:
+                screen = screen_stack.pop()
+            except IndexError:
+                break
+
+        screen_stack.current_screen = screen
+
+        if hasattr(screen, 'complete'):
+            shell.set_completer(screen.complete())
+
+        try:
+            screen = screen(shell, screen_stack)
+        except EOFError:
+            #if we recieve EOF we take this as a sign we want to move back a
+            #screen
+            shell.print_line()
+            screen = None
+
 if __name__ == "__main__":
     from menu import main_menu
-    shell = GameShell()
-    shell.run(main_menu)
+    mainloop(main_menu)
--- menu.py
+++ menu.py
@@ -13,9 +13,9 @@ class Menu(object):
     def add_menu_item(self, item):
         self.menu_items.append(item)
 
-    def __call__(self, shell):
+    def __call__(self, shell, stack):
         choice = shell.choose_from(self.name, self.menu_items)
-        shell.push(self)
+        stack.push(self)
         return choice
 
 class MenuItem(object):
@@ -26,30 +26,33 @@ class MenuItem(object):
     def __str__(self):
         return self.name
 
-    def __call__(self, shell):
-        return self.fn(shell)
+    def __call__(self, *args):
+        return self.fn(*args)
 
+#decorator
 def menu_item(name):
     def bind(fn):
         return MenuItem(name, fn)
     return bind
 
 @menu_item("Change Speed")
-def speed_change(shell):
+def speed_change(shell, stack):
     speed = shell.choose_from("Change Speed", ['slow', 'normal', 'fast'])
     shell.set_speed(speed)
     return None
 
-def new_game(shell):
+@menu_item("New Game")
+def new_game(shell, stack):
     shell.print_line("New Game not implemented")
     return None
 
-def load_game(shell):
+@menu_item("Load Game")
+def load_game(shell, stack):
     shell.print_line("Load Game not implemented")
     return None
 
-def back(shell):
-    shell.pop()
+def back(shell, stack):
+    stack.pop()
     return None
 
 settings_menu = Menu("Settings")
@@ -57,7 +60,7 @@ settings_menu.add_menu_item(speed_change)
 settings_menu.add_menu_item(MenuItem("Back", back))
 
 main_menu = Menu('Main Menu')
-main_menu.add_menu_item(MenuItem("Start New Game", new_game))
-main_menu.add_menu_item(MenuItem("Load Game", load_game))
+main_menu.add_menu_item(new_game)
+main_menu.add_menu_item(load_game)
 main_menu.add_menu_item(settings_menu)
 main_menu.add_menu_item(MenuItem("Exit", back))
